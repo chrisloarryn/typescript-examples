9477943a7945793385dae3fa5a2a5a4e933bcaf1
chrisloarryn <chrisloarryn@gmail.com>
2020-03-11 16:18:03 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { timer, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+// switch to new inner observable when source emits, emit result of project function
+timer(0,5000).pipe(
+  switchMap(
+    _ => interval(2000),
+    (outerValue, innerValue, outerIndex, innerIndex) => ({
+      outerValue,
+      innerValue,
+      outerIndex,
+      innerIndex
+    })
+  )
+)
+/*
+	Output:
+	{outerValue: 0, innerValue: 0, outerIndex: 0, innerIndex: 0}
+	{outerValue: 0, innerValue: 1, outerIndex: 0, innerIndex: 1}
+	{outerValue: 1, innerValue: 0, outerIndex: 1, innerIndex: 0}
+	{outerValue: 1, innerValue: 1, outerIndex: 1, innerIndex: 1}
+*/
+.subscribe(val => console.log(val));
